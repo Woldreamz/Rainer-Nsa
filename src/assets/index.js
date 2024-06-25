6455d95920aa830d133e055dfab8439c22accd73
Rainer Nsa <woldreamz@gmail.com>
2024-06-25 14:11:46 +0100

    City of God

---
+++ src/assets/index.js
@@ -0,0 +1,73 @@
+import logo from "./Rainer.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./woldreamz-inc-favicon-color.png";
+import shopify from "./woldreamz-inc-favicon-color.png";
+import starbucks from "./woldreamz-inc-favicon-color.png";
+import tesla from "./woldreamz-inc-favicon-color.png";
+
+import carrent from "./Gym.png";
+import jobit from "./Househunter.png";
+import tripguide from "./Draycore.png";
+
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+};
